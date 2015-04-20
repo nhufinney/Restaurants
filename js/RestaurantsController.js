174cfe392906b01111b1b285cb73c2040c10b424
Jill Kuchman & Nhu Finney <student@epicodus.com>
2015-04-20 15:41:26 -0700

    Add seperate search fields for name, type of food, address, price range

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,11 +1,13 @@
 bestRestaurants.controller("RestaurantsCtrl", function RestaurantsCtrl($scope) {
     $scope.restaurants = [];
     $scope.addRestaurant = function() {
-        $scope.restaurants.push({name: $scope.restaurantName});
-        $scope.restaurantName = null;
+        $scope.restaurants.push({name: $scope.restaurantName, typeFood: $scope.restaurantFoodType, address: $scope.restaurantLocation, priceRange: $scope.restaurantPriceRange});
+        $scope.restaurantName = $scope.restaurantFoodType = $scope.restaurantLocation = $scope.restaurantPriceRange = null;
     };
     $scope.deleteRestaurant = function(restaurant) {
     var index = $scope.restaurants.indexOf(restaurant);
     $scope.restaurants.splice(index, 1);
 };
+
+    
 });
