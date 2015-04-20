edeeba7e338de7913c0ac9b3821620b4c8b74a52
Jill Kuchman & Nhu Finney <student@epicodus.com>
2015-04-20 14:01:33 -0700

    Add forms to add new restaurant,search restaurants, and functionality to edit/delete restaurants?

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,6 +1,11 @@
 bestRestaurants.controller("RestaurantsCtrl", function RestaurantsCtrl($scope) {
     $scope.restaurants = [];
     $scope.addRestaurant = function() {
-
+        $scope.restaurants.push({name: $scope.restaurantName});
+        $scope.restaurantName = null;
     };
+    $scope.deleteRestaurant = function(restaurant) {
+    var index = $scope.restaurants.indexOf(restaurant);
+    $scope.restaurants.splice(index, 1);
+};
 });
